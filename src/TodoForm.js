d040fd9244790a4cc1b27a716b23d6df9bbadfea
Danielle Spencer <d.m.spencer.0486@gmail.com>
2020-06-07 13:29:26 -0500

    defined todo reducer

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,17 +1,17 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import TextField from "@material-ui/core/TextField";
 import Paper from "@material-ui/core/Paper";
 import useInputState from "./hooks/useInputState";
-import {TodosContext} from './context/todos.context'
+import { TodosContext } from "./context/todos.context";
 function TodoForm() {
   const [value, handleChange, reset] = useInputState("");
-  const {addTodo} = useContext(TodosContext)
+  const { dispatch } = useContext(TodosContext);
   return (
     <Paper style={{ margin: "1rem 0", padding: "0 1rem" }}>
       <form
-        onSubmit={e => {
+        onSubmit={(e) => {
           e.preventDefault();
-          addTodo(value);
+          dispatch({ type: "ADD", task: value });
           reset();
         }}
       >
